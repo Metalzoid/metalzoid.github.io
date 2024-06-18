35810fb8a4cdd2dbc780da4a82f751c0074bba78
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-06-18 11:14:29 +0200

    after pull

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      title: "Gagnaire Development",
+      title: "Florian Gagnaire Development",
     },
   },
   build: {
